656d395965e322f0402f06f8c873f650faded1b8
nabiilawidya <nabiilawidya@7702@gmail.com>
2024-08-14 21:02:31 +0700

    first activity

--- app/src/main/java/com/nabiilawidya/suitmediamsib/ui/MainActivity.kt
+++ app/src/main/java/com/nabiilawidya/suitmediamsib/ui/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.nabiilawidya.suitmediamsib.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.nabiilawidya.suitmediamsib.R
+import com.nabiilawidya.suitmediamsib.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        binding.checkButton.setOnClickListener {
+            val sentence = binding.palindromeInput.text.toString()
+            val isPalindrome = isPalindrome(sentence)
+            if (isPalindrome) {
+                Toast.makeText(this, "Palindrome", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Not Palindrome", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.nextButton.setOnClickListener {
+            val name = binding.nameInput.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("name", name)
+            startActivity(intent)
+        }
+    }
+
+    private fun isPalindrome(sentence: String): Boolean {
+        val palindrome = sentence.replace("\\s".toRegex(), "").lowercase()
+        return palindrome == palindrome.reversed()
     }
 }
